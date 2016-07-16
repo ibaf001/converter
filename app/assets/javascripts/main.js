9146686e736d67826c87443ea819c7722da1bea6
ibo <ibobafumba@gmail.com>
2016-07-16 01:42:49 -0400

    Add event test for javascript

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,4 +1,11 @@
-convert(){
-  document.getElementById('demo').innerHTML=Date()
 
+
+function convert() {
+    //document.getElementById("r1").value = document.getElementById("s1").value ;
+    var a = document.getElementById("s1").value
+    var b = document.getElementById("s2").value
+    if(a == b){
+      alert(a +' '+b);
+    }
+    
 }
