729d4a9682ae5171d01ad55595c2823be9f3855d
ibo <ibo.b@tcs.com>
2016-07-18 14:16:18 -0400

    Add length time and volume pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,9 +3,27 @@ class StaticPagesController < ApplicationController
   end
   
   def temp
+    @text = 'Temperature'
     @units = ['celsius','fahrenheit','kelvin']
   end
 
-  def temp_convert
+  def lengths
+    @text = 'Length'
+    @units = ['meters','decimeters','centimeters','inches']
   end
+  
+  def time
+    @text = 'Time'
+    @units = ['secondes','minutes','hours','days']
+  end
+
+  def volume
+    @text = 'Volume'
+    @units = ['liters','gallons','cubic feet','microliters']
+  end
+
+  def reset
+    redirect_to request.referrer 
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   get 'temp' =>'static_pages#temp'
-  post 'temp_convert' => 'static_pages#temp_convert'
+  get 'length' =>'static_pages#lengths'
+  get 'time' =>'static_pages#time'
+  get 'volume' =>'static_pages#volume'
+  get 'reset' =>'static_pages#reset'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
