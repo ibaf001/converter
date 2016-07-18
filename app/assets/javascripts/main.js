1aba4bb311e9db5aa6b85de30b2cbb7ab58eaad8
ibo <ibobafumba@gmail.com>
2016-07-17 21:48:13 -0400

    Add oneKeyUp event

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,11 +1,18 @@
 
 
 function convert() {
-    //document.getElementById("r1").value = document.getElementById("s1").value ;
+    //document.getElementById("r1").value = document.getElementById("s1").value ; //fahrenheit //
     var a = document.getElementById("s1").value
     var b = document.getElementById("s2").value
-    if(a == b){
-      alert(a +' '+b);
+    var v1 = parseInt(document.getElementById("r1").value)
+    if(a =='celsius' && b == 'fahrenheit'){
+       document.getElementById("r2").value = ((v1 * 9)/5) + 32;
     }
     
+}
+
+function convert_left(){
+}
+
+function convert_right(){
 }
