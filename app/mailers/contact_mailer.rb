f47a17928879d2fb7e4ead3a1fe47cc72b4d0dfa
ibo <ibo.b@tcs.com>
2016-07-21 14:36:35 -0400

    Changes from so that it is variable

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -5,6 +5,6 @@ class ContactMailer < ApplicationMailer
     @name = name
     #from: "olala@gmail.com"
     @message = message
-    mail to: "ibobafumba@gmail.com", subject: "units conversion not working"
+    mail to: "ibobafumba@gmail.com", subject: "units conversion not working", from: email
   end
 end
